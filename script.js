4dc7f9ad298e7ff0f266b84ab036990857e9d45f
pinto64 <alejandropintobenavente@gmail.com>
2026-02-20 20:50:35 +0100

    Añadida decoración y audios

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let dinero = 0.01
 
 function irCasa() {
     document.getElementById("escena").style.backgroundImage = "url(img/casa.png)";
+
+    document.getElementById("bobesponja").style.display = "block";
     document.getElementById("casa").style.display = "none";
-        document.getElementById("bus").style.display = "none";
+    document.getElementById("bus").style.display = "none";
+    document.getElementById("exit").style.display = "none";
+
     document.getElementById("parrafo").innerHTML = "cuanta gente"
+
 }
 
 function irParada() {
     document.getElementById("escena").style.backgroundImage = "url(img/paradaBus.png)";
+    document.getElementById("bobesponja").style.display = "none";
     document.getElementById("casa").style.display = "block";
-            document.getElementById("bus").style.display = "block";
-
+    document.getElementById("bus").style.display = "block";
     document.getElementById("parrafo").innerHTML = "en la parada"
 
 }
@@ -31,17 +36,35 @@ function dentroCasa() {
     document.getElementById("escena").style.backgroundImage = "url(img/salon.png)";
     document.getElementById("parada").style.display = "none";
     document.getElementById("dentroCasa").style.display = "none";
+    document.getElementById("exit").style.display = "block";
+    document.getElementById("bobesponja").style.display = "none";
+    document.getElementById("gato").style.display = "block";
+
     document.getElementById("parrafo").innerHTML = "estoy en el slon"
 
+
 }
 
-function exit(){
+function exit() {
     document.getElementById("escena").style.backgroundImage = "url(img/casa.png)";
-        document.getElementById("dentroCasa").style.display = "block";
+    document.getElementById("dentroCasa").style.display = "block";
     document.getElementById("parada").style.display = "block";
-document.getElementById("parrafo").innerHTML = "cuanta gente"
+    document.getElementById("parrafo").innerHTML = "cuanta gente"
+    document.getElementById("bobesponja").style.display = "block";
+    document.getElementById("gato").style.display = "none";
 
 
 }
+
+function sonidoBob() {
+    var audio = document.getElementById("miAudioBob");
+    audio.play();
+}
+
+function miau() {
+    var audio = document.getElementById("miau");
+    audio.play();
+}
+
 
 
