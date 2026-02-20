f71cb17b34adc72fd6f20adcbfc051daff9db51f
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-02-20 17:42:32 +0100

    Añadidas 3 primeras escenas (versión base)

---
+++ script.js
@@ -0,0 +1,47 @@
+let dinero = 0.01
+
+function irCasa() {
+    document.getElementById("escena").style.backgroundImage = "url(img/casa.png)";
+    document.getElementById("casa").style.display = "none";
+        document.getElementById("bus").style.display = "none";
+    document.getElementById("parrafo").innerHTML = "cuanta gente"
+}
+
+function irParada() {
+    document.getElementById("escena").style.backgroundImage = "url(img/paradaBus.png)";
+    document.getElementById("casa").style.display = "block";
+            document.getElementById("bus").style.display = "block";
+
+    document.getElementById("parrafo").innerHTML = "en la parada"
+
+}
+
+
+
+function bus() {
+    if (dinero == 1) {
+        document.getElementById("escena").style.backgroundImage = "url(img/viviendoenlacalle.png)";
+        document.getElementById("parrafo").innerHTML = "en el autobus"
+    } else {
+        document.getElementById("parrafo").innerHTML = "pobre"
+    }
+}
+
+function dentroCasa() {
+    document.getElementById("escena").style.backgroundImage = "url(img/salon.png)";
+    document.getElementById("parada").style.display = "none";
+    document.getElementById("dentroCasa").style.display = "none";
+    document.getElementById("parrafo").innerHTML = "estoy en el slon"
+
+}
+
+function exit(){
+    document.getElementById("escena").style.backgroundImage = "url(img/casa.png)";
+        document.getElementById("dentroCasa").style.display = "block";
+    document.getElementById("parada").style.display = "block";
+document.getElementById("parrafo").innerHTML = "cuanta gente"
+
+
+}
+
+
