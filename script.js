76ca5a62d31b72c52c8a65a6031767f70520668a
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-02-24 20:20:08 +0100

    Creada estructura de diálogos minijuego Fernando

--- script.js
+++ script.js
@@ -74,13 +74,14 @@ function irPatio() {
     document.getElementById("gato").style.display = "none";
     document.getElementById("puertaExit").style.display = "none";
     document.getElementById("minijuegoNano").style.display = "block";
+    document.getElementById("volverDentro").style.display = "block";
 }
 
 function volverDentro() {
     document.getElementById("escena").style.backgroundImage = "url(img/salon.png)";
     document.getElementById("gato").style.display = "block";
     document.getElementById("puertaExit").style.display = "block";
-
+    document.getElementById
 }
 
 
@@ -112,17 +113,23 @@ function pasarDialogo(textContentHabla, styleColor, textContentParrafo) {
     document.getElementById("habla").textContent = textContentHabla;
     document.getElementById("habla").style.color = styleColor;
     document.getElementById("parrafo").textContent = textContentParrafo;
+    document.getElementById("habla").style.fontWeight = "bold";
 }
 
 let pasoNano = 0;
+let puedeJugarNano = true;
 
 function minijuegoNano() {
-    document.getElementById("escena").style.backgroundImage = "url(img/fernandoAlonso.png)";
-    document.getElementById("javi").src = "img/javisorprendido.png";
-    document.getElementById("minijuegoNano").style.display = "none";
-    pasoNano = 0;
-    document.getElementById("texto").onclick = avanzarNano;
-    avanzarNano(); 
+    if (puedeJugarNano) {
+        document.getElementById("escena").style.backgroundImage = "url(img/fernandoAlonso.png)";
+        document.getElementById("javi").src = "img/javisorprendido.png";
+        document.getElementById("minijuegoNano").style.display = "none";
+        pasoNano = 0;
+        document.getElementById("texto").onclick = avanzarNano;
+        avanzarNano();
+    } else {
+        document.getElementById("texto").innerText = "Ya has jugado a este minijuego";
+    }
 }
 
 function avanzarNano() {
@@ -130,16 +137,30 @@ function avanzarNano() {
         pasarDialogo("Javier: ", "darkblue", "¡PERO SI ES FERNANDO ALONSO!");
     }
     else if (pasoNano == 1) {
-        pasarDialogo("Fernando Alonso: ", "darkgreen", "Hola macho el motor Honda no corre");
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "Hola muchacho. ¿Te gusta mi nueva barbacoa?");
     }
     else if (pasoNano == 2) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "Es el Aston Martin del 2026, con el nuevo motor Honda");
+    }
+    else if (pasoNano == 3) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "No puede acabar una carrera");
+    }
+    else if (pasoNano == 4) {
         pasarDialogo("Fernando Alonso: ", "darkgreen", "El motol ase balbacoa");
     }
-    else {
-        document.getElementById("texto").onclick = null; // termina diálogo
-        // Habría que finalizar minijuego y volver a escena anterior.
-        // Quitarle la posibilidad al jugador de que vuelva a jugar el minijuego
+    else if (pasoNano == 5) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "El motol ase balbacoa");
     }
-
+    else if (pasoNano == 6) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "El motol ase balbacoa");
+    }
+    
+    else {
+        document.getElementById("texto").onclick = null; // Terminar diálogo
+        // Finalizar el minijuego y volver a la escena anterior
+        irPatio();
+        // Quitarle la posibilidad al jugador de que vuelva a jugar el minijuego
+        puedeJugarNano = false;
+    }
     pasoNano++;
 }
