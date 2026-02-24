e4306288dd2e5b26449fedcbbc0e42e008e470ab
Javierhb16 <javier.hidalgo1605@gmail.com>
2026-02-24 16:21:32 +0100

    Base minijuego Alonso y función para diálogos

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function irCasa() {
     document.getElementById("parada").style.display = "block";
     document.getElementById("casa").style.display = "none";
     document.getElementById("bus").style.display = "none";
-    document.getElementById("exit").style.display = "none";
+    document.getElementById("puertaExit").style.display = "none";
     document.getElementById("patio").style.display = "block";
-    document.getElementById("parrafo").innerHTML = "cuanta gente"
+    document.getElementById("parrafo").innerText = "cuanta gente"
 
 }
 
@@ -22,7 +22,7 @@ function irParada() {
     document.getElementById("dentroCasa").style.display = "none";
     document.getElementById("casa").style.display = "block";
     document.getElementById("bus").style.display = "block";
-    document.getElementById("parrafo").innerHTML = "en la parada"
+    document.getElementById("parrafo").innerText = "en la parada"
 
 }
 
@@ -31,9 +31,9 @@ function irParada() {
 function bus() {
     if (dinero == 1) {
         document.getElementById("escena").style.backgroundImage = "url(img/viviendoenlacalle.png)";
-        document.getElementById("parrafo").innerHTML = "en el autobus"
+        document.getElementById("parrafo").innerText = "en el autobus"
     } else {
-        document.getElementById("parrafo").innerHTML = "pobre"
+        document.getElementById("parrafo").innerText = "pobre"
     }
 }
 
@@ -41,21 +41,21 @@ function dentroCasa() {
     document.getElementById("escena").style.backgroundImage = "url(img/salon.png)";
     document.getElementById("parada").style.display = "none";
     document.getElementById("dentroCasa").style.display = "none";
-    document.getElementById("exit").style.display = "block";
+    document.getElementById("puertaExit").style.display = "block";
     document.getElementById("bobesponja").style.display = "none";
     document.getElementById("gato").style.display = "block";
-    document.getElementById("parrafo").innerHTML = "estoy en el slon"
+    document.getElementById("parrafo").innerText = "estoy en el slon"
 }
 
-function exit() {
+function salirCasa() {
     document.getElementById("escena").style.backgroundImage = "url(img/casa.png)";
     document.getElementById("dentroCasa").style.display = "block";
     document.getElementById("parada").style.display = "block";
     document.getElementById("bobesponja").style.display = "block";
     document.getElementById("gato").style.display = "none";
-    document.getElementById("exit").style.display = "none";
+    document.getElementById("puertaExit").style.display = "none";
 
-    document.getElementById("parrafo").innerHTML = "cuanta gente"
+    document.getElementById("parrafo").innerText = "cuanta gente"
 
 }
 
@@ -72,21 +72,74 @@ function miau() {
 function irPatio() {
     document.getElementById("escena").style.backgroundImage = "url(img/patioTrasero.png)";
     document.getElementById("gato").style.display = "none";
-    document.getElementById("exit").style.display = "none";
+    document.getElementById("puertaExit").style.display = "none";
     document.getElementById("minijuegoNano").style.display = "block";
 }
 
 function volverDentro() {
     document.getElementById("escena").style.backgroundImage = "url(img/salon.png)";
     document.getElementById("gato").style.display = "block";
-    document.getElementById("exit").style.display = "block";
+    document.getElementById("puertaExit").style.display = "block";
 
 }
 
+
+// function minijuegoNano() {
+//     document.getElementById("escena").style.backgroundImage = "url(img/fernandoAlonso.png)";
+//     document.getElementById("javi").src = "img/javisorprendido.png";
+//     document.getElementById("minijuegoNano").display = "none";
+
+//     document.getElementById("habla").textContent = "Javier: ";
+//     document.getElementById("habla").style.color = "darkblue";
+//     document.getElementById("habla").style.fontWeight = "bold";
+//     document.getElementById("parrafo").textContent = "¡PERO SI ES FERNANDO ALONSO!";
+
+//     // document.getElementById("texto").onclick = function() {
+//     //     pasarDialogo("Fernando Alonso: ", "darkgreen", "Hola macho el motor Honda no corre");
+//     // }
+
+//     // document.getElementById("texto").onclick = function() {
+//     //     pasarDialogo("Fernando Alonso: ", "darkgreen", "el motol ase balbacoa");
+//     // }
+
+//     // document.getElementById("habla").textContent = "Fernando Alonso: ";
+//     // document.getElementById("habla").style.color = "darkgreen";
+//     // document.getElementById("parrafo").textContent = "ola";
+
+// }
+
+function pasarDialogo(textContentHabla, styleColor, textContentParrafo) {
+    document.getElementById("habla").textContent = textContentHabla;
+    document.getElementById("habla").style.color = styleColor;
+    document.getElementById("parrafo").textContent = textContentParrafo;
+}
+
+let pasoNano = 0;
+
 function minijuegoNano() {
     document.getElementById("escena").style.backgroundImage = "url(img/fernandoAlonso.png)";
-        document.getElementById("habla").innerHTML = "Fernando Alonso:"
-    document.getElementById("habla").style.color = "darkgreen";
-    document.getElementById("habla").style.fontWeight = "bold";
-    
+    document.getElementById("javi").src = "img/javisorprendido.png";
+    document.getElementById("minijuegoNano").style.display = "none";
+    pasoNano = 0;
+    document.getElementById("texto").onclick = avanzarNano;
+    avanzarNano(); 
+}
+
+function avanzarNano() {
+    if (pasoNano == 0) {
+        pasarDialogo("Javier: ", "darkblue", "¡PERO SI ES FERNANDO ALONSO!");
+    }
+    else if (pasoNano == 1) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "Hola macho el motor Honda no corre");
+    }
+    else if (pasoNano == 2) {
+        pasarDialogo("Fernando Alonso: ", "darkgreen", "El motol ase balbacoa");
+    }
+    else {
+        document.getElementById("texto").onclick = null; // termina diálogo
+        // Habría que finalizar minijuego y volver a escena anterior.
+        // Quitarle la posibilidad al jugador de que vuelva a jugar el minijuego
+    }
+
+    pasoNano++;
 }
